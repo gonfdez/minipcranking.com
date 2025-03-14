ce614e052f17e3d48a6f456188aa159859e1cead
gon <gonefdez@gmail.com>
2025-03-15 00:53:21 +0100

    Update

--- layouts/ListLayout.tsx
+++ layouts/ListLayout.tsx
@@ -34,9 +34,6 @@ export default function ListLayout({
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pt-6 pb-8 md:space-y-5">
-          <h1 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 dark:text-gray-100">
-            {title}
-          </h1>
           <div className="relative">
             <label>
               <span className="sr-only">Search articles</span>
