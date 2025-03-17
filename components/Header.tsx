90138d5a7d3553848368f45fde5c5bdbb16ca5f5
gon <gonefdez@gmail.com>
2025-03-18 00:31:13 +0100

    Update

--- components/Header.tsx
+++ components/Header.tsx
@@ -22,9 +22,7 @@ const Header = () => {
             </span>
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
+            <div className="h-6 text-2xl font-semibold sm:block">{siteMetadata.headerTitle}</div>
           ) : (
             siteMetadata.headerTitle
           )}
