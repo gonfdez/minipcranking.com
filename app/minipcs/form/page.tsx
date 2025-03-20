62eaa5e6c35040d18e7f81649986a5e23be3e9fd
gon <gonefdez@gmail.com>
2025-03-20 22:57:00 +0100

    Update

--- app/minipcs/form/page.tsx
+++ app/minipcs/form/page.tsx
@@ -1,5 +1,10 @@
 import MiniPcForm from '@/components/MiniPcForm/MiniPcForm'
 
+export const metadata = {
+  title: 'Mini PC Formulario',
+  robots: 'noindex, nofollow',
+}
+
 export default function MiniPcFormPage() {
   return <MiniPcForm />
 }
