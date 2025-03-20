af4863e3e8f8bb15b0fc0905eac5ed6618399199
gon <gonefdez@gmail.com>
2025-03-20 22:53:39 +0100

    Update

--- app/minipcs/form/schema.tsx
+++ app/minipcs/form/schema.tsx
@@ -1,12 +1,17 @@
+import { MINI_PC_BRAND } from '@/data/minipcs/brands'
 import { RJSFSchema } from '@rjsf/utils'
 
 export const miniPcSchema: RJSFSchema = {
   title: 'Mini PC Form',
   type: 'object',
   properties: {
-    id: { type: 'string', title: 'ID' },
+    id: { type: 'string', title: 'ID', readOnly: true },
     title: { type: 'string', title: 'Título' },
-    brand: { type: 'string', title: 'Marca' },
+    brand: {
+      type: 'string',
+      title: 'Marca',
+      enum: Object.values(MINI_PC_BRAND),
+    },
     model: { type: 'string', title: 'Modelo' },
     description: { type: 'string', title: 'Descripción' },
     imgSrc: { type: 'string', title: 'URL de la imagen principal del producto', format: 'uri' },
@@ -14,7 +19,11 @@ export const miniPcSchema: RJSFSchema = {
       type: 'object',
       title: 'Procesador',
       properties: {
-        brand: { type: 'string', title: 'Marca' },
+        brand: {
+          type: 'string',
+          title: 'Marca',
+          enum: Object.values(MINI_PC_BRAND),
+        },
         model: { type: 'string', title: 'Modelo' },
         cores: { type: 'integer', title: 'Núcleos' },
         threads: { type: 'integer', title: 'Hilos' },
@@ -35,7 +44,15 @@ export const miniPcSchema: RJSFSchema = {
       title: 'Gráficos',
       properties: {
         integrated: { type: 'boolean', title: 'Integrado', default: true },
-        brand: { type: 'string', title: 'Marca' },
+        supportExternalGraphicsCard: {
+          type: 'boolean',
+          title: 'Soporta tarjeta gráfica externa',
+        },
+        brand: {
+          type: 'string',
+          title: 'Marca',
+          enum: Object.values(MINI_PC_BRAND),
+        },
         model: { type: 'string', title: 'Modelo' },
         frequencyMHz: { type: 'number', title: 'Frecuencia (MHz)' },
         maxTOPS: { type: 'number', title: 'TOPS máximo' },
@@ -101,6 +118,20 @@ export const miniPcSchema: RJSFSchema = {
     weightKg: { type: 'number', title: 'Peso (Kg)' },
     powerConsumptionW: { type: 'number', title: 'Consumo de Energía (W)' },
     releaseYear: { type: 'integer', title: 'Año de lanzamiento' },
+    ports: {
+      type: 'object',
+      title: 'Puertos',
+      properties: {
+        imageSrc: { type: 'string', title: 'URL de la imagen de los puertos', format: 'uri' },
+        usb4: { type: 'integer', title: 'USB4 (Unidades)' },
+        usb3: { type: 'integer', title: 'USB3 (Unidades)' },
+        usb2: { type: 'integer', title: 'USB2 (Unidades)' },
+        usbC: { type: 'integer', title: 'USB-C (Unidades)' },
+        ethernet: { type: 'integer', title: 'Ethernet (Unidades)' },
+        audioJack: { type: 'boolean', title: 'Jack de audio' },
+        sdCardReader: { type: 'boolean', title: 'Lector de tarjetas SD' },
+      },
+    },
     variants: {
       type: 'array',
       title: 'Variante',
