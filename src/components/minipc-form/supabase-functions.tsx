6e8014b9580049e2d71bcb1ada57587e55754a72
Gonzalo Fernández Suárez <83967769+gonfdez@users.noreply.github.com>
2025-08-13 11:25:12 +0000

    mini pc form supabase-functions clean useless code

--- src/components/minipc-form/supabase-functions.tsx
+++ src/components/minipc-form/supabase-functions.tsx
@@ -1,10 +1,6 @@
-import { createClient } from "@supabase/supabase-js";
+import { supabase } from "@/lib/supabaseClient";
 import { FormData } from "./MiniPCForm";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 interface CreateMiniPCResponse {
   success: boolean;
